7028fcee27bc94d65eb48f7a989a3575cbede438
Omeed Tehrani <61725820+omeedcs@users.noreply.github.com>
2022-08-18 11:19:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'InvestopediaApi',
-  packages = ['InvestopediaApi'],
-  version = '1.1',
-  description = 'An API for Investopedia\'s paper trading simulator',
-  author = 'Kirk Thaker',
-  author_email = 'kirkthaker66@gmail.com',
-  url = 'https://github.com/kirkthaker/investopedia-trading-api',
+  name = 'InvestopediaApiFixed',
+  packages = ['InvestopediaApiFixed'],
+  version = '1.2',
+  description = 'An API for Investopedia\'s paper trading simulator (fixed by Omeed)',
+  author = 'Kirk Thaker and Omeed Tehrani',
+  author_email = 'omeed26@gmail.com',
+  url = 'https://github.com/omeedcs/investopedia-trading-api.git',
   download_url = 'https://github.com/kirkthaker/investopedia-trading-api/archive/1.1.tar.gz',
   keywords = ['trading', 'finance', 'investopedia', 'algorithmic'],
   classifiers = [],
